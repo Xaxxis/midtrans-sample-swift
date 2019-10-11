79c53b8c69deff0c43158afe8948b7fc747c0ddc
Xaxxis <zaki.ibrahim@midtrans.com>
2019-10-11 14:45:21 +0700

    Initial Commit

---
+++ midtrans-sample-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  midtrans-sample-swift
+//
+//  Created by Zaki Ibrahim on 11/10/19.
+//  Copyright © 2019 Zaki Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
